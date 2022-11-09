d5aadbdc8ce1ce2931e1301b03485edc8eacfbc9
Lucas Jellema <lucasjellema@gmail.com>
2022-11-09 20:26:54 +0000

    add support for getting and saving state in statestore

--- lab7-java-and-dapr/async-java/src/main/java/com/myacme/service/controller/NameConsumerAndProcessorServiceController.java
+++ lab7-java-and-dapr/async-java/src/main/java/com/myacme/service/controller/NameConsumerAndProcessorServiceController.java
@@ -1,7 +1,10 @@
 package com.myacme.service.controller;
 
 import io.dapr.Topic;
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
 import io.dapr.client.domain.CloudEvent;
+import io.dapr.client.domain.State;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +21,7 @@ import reactor.core.publisher.Mono;
 public class NameConsumerAndProcessorServiceController {
     
     private static final Logger logger = LoggerFactory.getLogger(NameConsumerAndProcessorServiceController.class);
+    private static final String STATE_STORE_NAME = "statestore";
 
     @Topic(name = "names", pubsubName = "pubsub")
     @PostMapping(path = "/names", consumes = MediaType.ALL_VALUE)
@@ -25,11 +29,23 @@ public class NameConsumerAndProcessorServiceController {
         return Mono.fromSupplier(() -> {
             try {
                 logger.info("Subscriber received from Names: " + cloudEvent.getData().getName());
+                int nameOccurrenceCount = retrieveIncrementSave(cloudEvent.getData().getName());
+                logger.info("Received message with name " + cloudEvent.getData().getName() + " that has occurred a total of " +  nameOccurrenceCount+ " times");
                 return ResponseEntity.ok("SUCCESS");
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    private int retrieveIncrementSave(String name) {
+        DaprClient client = new DaprClientBuilder().build();
+        Mono<State<String>> result = client.getState(STATE_STORE_NAME, name, String.class);
+        // if a result was found, it is turned to an int and incremented by one. if no result was found, this is the first occurrence and the value is set to one
+        int numberOfOccurrences = result.block().getValue() == null ? 1 : Integer.parseInt(result.block().getValue())+1;
+        client.saveState(STATE_STORE_NAME, name, Integer.toString(numberOfOccurrences)).block();
+        Mono<State<String>> result2 = client.getState(STATE_STORE_NAME, name, String.class);
+        return Integer.parseInt(result2.block().getValue());
     }
 }
 

c804e8acd124f2ad4798e63804aafcbbaec75053
Lucas Jellema <lucasjellema@gmail.com>
2022-11-10 08:17:34 +0100

    add lab8 hello-world-async application

---
+++ lab8-dapr-kafka-advanced/hello-world-async-dapr/app.js
@@ -0,0 +1,54 @@
+import { DaprClient, DaprServer, CommunicationProtocolEnum } from '@dapr/dapr';
+
+const daprHost = "127.0.0.1"; 
+const daprPort = process.env.DAPR_HTTP_PORT ;
+const serverHost = "127.0.0.1";
+const serverPort = process.env.APP_PORT ; 
+
+const TOPIC_NAME  = "names"
+const NAMES_INPUT_BINDING_NAME = "names-input";
+
+const daprclient = new DaprClient(daprHost, daprPort);
+const serviceStoreName = "statestore";
+
+start().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
+
+async function start() {
+    const daprserver = new DaprServer(
+        serverHost, 
+        serverPort, 
+        daprHost, 
+        daprPort, 
+        CommunicationProtocolEnum.HTTP
+    );
+    //Subscribe to an input binding to trigger the function
+    await server.binding.receive(NAMES_INPUT_BINDING_NAME,  async (name) => { // function to be invoked whenever a message is received from the sidecar
+        console.log(`Input trigger received: ${name}`)
+        let nameOccurrenceCount = await retrieveIncrementSave(name);
+        console.log(`Received message from input binding ${NAMES_INPUT_BINDING_NAME} with name ${name} that has occurred a total of ${nameOccurrenceCount} times`)
+    });
+    await daprserver.start();
+}
+
+async function retrieveIncrementSave(key) {
+    let value = 0;
+    let response = await daprclient.state.get(serviceStoreName, key );
+    if (!response) {
+        value = 1;
+    } else {
+        value = parseInt(response) + 1;
+    }
+    response = await daprclient.state.save(serviceStoreName, [
+        {
+            key: key,
+            value: `${value}`
+        }
+    ]);
+    return value;
+}
+
+
+
---
+++ lab8-dapr-kafka-advanced/hello-world-async-dapr/front-app.js
@@ -0,0 +1,51 @@
+import { DaprClient, CommunicationProtocolEnum } from '@dapr/dapr';
+import * as http from 'http';
+import * as url from 'url';
+
+const PORT = process.env.APP_PORT || "3200"
+const daprHost = "127.0.0.1"; 
+const daprPort = process.env.DAPR_HTTP_PORT ;
+
+const TOPIC_NAME  = "names"
+const NAMES_OUTPUT_BINDING_NAME = "names-output";
+const NAMES_OUTPUT_BINDING_OPERATION = "create";
+const client = new DaprClient(daprHost, process.env.DAPR_HTTP_PORT, CommunicationProtocolEnum.HTTP);
+
+const daprclient = new DaprClient(daprHost, daprPort, CommunicationProtocolEnum.HTTP)
+
+const serviceStoreName = "statestore";
+
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET' && req.url !== '/dapr/config') { // note: the first GET request sent to the application is from the sidecar at path /dapr/config; we do not currently have a proper implementation for that path
+        const query = url.parse(req.url, true).query
+        let key = query.name ? query.name : "World"
+        let text
+ 
+        if (key != null) {
+            try {
+                let value = 0;
+                let response = await daprclient.state.get(serviceStoreName, key ); // try to find entry in statestore with this key
+                if (!response) {
+                    value = 1; // if it was not found, the name is now mentioned for the first time
+                } else {
+                    value = parseInt(response) + 1; // if it was found, the response indicates how many times before now the name was mentioned
+                }
+                text = `Hello ${key} - greeting #${value}`
+
+                //Using Dapr SDK to invoke output binding (that results in a message being published on a Kafka Topic - but nothing here suggests that)
+                const result = await client.binding.send(NAMES_OUTPUT_BINDING_NAME, NAMES_OUTPUT_BINDING_OPERATION, key);
+
+                console.log(`Published name ${key} to topic ${TOPIC_NAME} with result ${result}`)
+
+            } catch (e) { text = `Exception occurred ${e}` }
+        }
+
+        res.setHeader('Content-Type', 'text/html');
+        res.end(text)
+    }
+    res.end()
+})
+
+server.listen(PORT);
+console.log(`HTTP Server is listening at port ${PORT} for HTTP GET requests`)
+
